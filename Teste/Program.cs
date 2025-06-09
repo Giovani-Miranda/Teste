a52fb77649b86ae1faa25ede40389445d82ce7e7
Giovani-Miranda <giovanimiranda01@gmail.com>
2025-06-09 15:11:51 -0300

    first commit

---
+++ Teste/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace teste;
+
+public class Produto
+{
+    public int Id { get; }
+    public string Nome { get; }
+    public decimal Preco { get; }
+    public string Categoria { get; }
+
+    public Produto(int id, string nome, decimal preco, string categoria)
+    {
+        if (id <= 0 || preco <= 0 || string.IsNullOrWhiteSpace(nome) || string.IsNullOrWhiteSpace(categoria))
+            throw new ArgumentException("Dados inválidos para o produto");
+
+        Id = id;
+        Nome = nome;
+        Preco = preco;
+        Categoria = categoria;
+    }
+}
